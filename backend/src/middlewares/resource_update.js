7f7682d778faa4f897f037cb684ca7dc4276808b
Vona Csaba <vcsneu@gmail.com>
2021-02-22 16:35:59 +0100

    Revert "Backend server problémák"
    This reverts commit a216c3914a129b69dcffc4740485e69b3589f6fd.

--- backend/src/middlewares/resource_update.js
+++ backend/src/middlewares/resource_update.js
@@ -7,10 +7,12 @@ let database = [{
     {
         type: 'gold',
         amount: 1,
-        generation: 1,
+        generation: 2,
         updatedAt: 1613994048,
     },
 ];
+
+export default Updater();
 
 database.forEach(element => {
     let dateNow = Date.now() / 1000;
