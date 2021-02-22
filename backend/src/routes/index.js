278dd566bb10c7979d82da9bf69322b43b89f4a2
Vona Csaba <vcsneu@gmail.com>
2021-02-22 16:36:10 +0100

    Revert "Kingdom Route elkezdve"
    This reverts commit f0c55979ba96cf33146dce95b353f26ca4679824.

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,5 @@
 import apiRouter from './api.routes';
 import systemRouter from './system.routes';
-import kingdomRouter from './kingdom.routes';
 
 export const api = apiRouter;
 export const system = systemRouter;
-export const kingdom = kingdomRouter;
