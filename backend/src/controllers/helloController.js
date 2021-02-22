be4076765fd693ce55fdfeb683edb8b6275c3a81
Vona Csaba <vcsneu@gmail.com>
2021-02-22 16:36:08 +0100

    Revert "Kingdom Route: Contoller hozzáadva"
    This reverts commit cf7d416f1fe1a2655844cced2370e021fdaac05d.

--- backend/src/controllers/helloController.js
+++ backend/src/controllers/helloController.js
@@ -1,9 +1,9 @@
 import { helloService } from '../services';
 
 export const helloController = {
-    asyncget(req, res) {
-        let data = await helloService.getHelloWorld();
+  async get(req, res) {
+    let data = await helloService.getHelloWorld();
 
-        res.status(200).json(data);
-    },
+    res.status(200).json(data);
+  },
 };
--- backend/src/routes/kingdom.routes.js
+++ backend/src/routes/kingdom.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const cors = require('cors');
-import { resourceController } from '../controllers';
+import { resource } from '../controllers';
 
 const router = express.Router();
 
 router.use(cors());
 router.use(express.json());
 
-router.get('/resource', resourceController.get);
+router.get('/resource', helloController.get);
 
 export default router;
