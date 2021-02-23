160145ca10e94d8ae0ec18796507992103e85b69
Babinszky Dániel <daniel.babinszky@gmail.com>
2021-02-23 14:31:22 +0100

    added register backend, thx a bunch guys

--- backend/src/routes/api.routes.js
+++ backend/src/routes/api.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const cors = require('cors');
-import { helloController } from '../controllers';
+import { helloController, registerController } from '../controllers';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.use(cors());
 router.use(express.json());
 
 router.get('/hello', helloController.get);
+router.post('/register', registerController.post);
 
 export default router;
