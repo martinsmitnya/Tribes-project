55b6259497368971d9e7b5a7163c7946202eb152
Vona Csaba <vcsneu@gmail.com>
2021-02-23 15:36:08 +0100

    Update Resource Logik müködik, kerekítés kérdéses

--- backend/src/middlewares/resource_update.js
+++ backend/src/middlewares/resource_update.js
@@ -1,17 +1,36 @@
 import { db } from '../data/connection';
 
 export function updatedResource(resource) {
+  let output = [];
   resource.then(result => {
-    result[0].amount = 1;
-    db.query('UPDATE resources SET amount=? WHERE type=?', [
-      result[0].amount,
-      'food',
+    output = Update(result);
+    db.query('UPDATE resources SET amount=?, updatedAt=? WHERE type=?', [
+      output[0].amount,
+      output[0].updatedAt,
+      output[0].type,
+    ]);
+    db.query('UPDATE resources SET amount=?, updatedAt=? WHERE type=?', [
+      output[1].amount,
+      output[1].updatedAt,
+      output[1].type,
     ]);
   });
   return resource;
 }
 
 function Update(input) {
-  let currentDate = new Date();
-  input.map(result => {});
+  let currentDate = Math.floor(Date.now() / 1000);
+  let output = [];
+  input.forEach(element => {
+    let amount = element.amount;
+    element.amount +=
+      element.generation * Math.floor((currentDate - element.updatedAt) / 60);
+
+    if (amount != element.amount) {
+      element.updatedAt = currentDate;
+    }
+
+    output.push(element);
+  });
+  return output;
 }
