7c0b850c73921b4ac1bcca038afeb447f658482f
Vona Csaba <vcsneu@gmail.com>
2021-02-23 16:09:01 +0100

    frontend test

--- frontend/src/Components/Login/Header.js
+++ frontend/src/Components/Login/Header.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import './Header.css';
 function Header() {
-
   function handleSubmit(event) {
     event.preventDefault();
     //Fetch comes here
-
   }
-
-  
 
   return (
     <div className="Header">
-
-      <div class='navbarWrapper'>
+      <div class="navbarWrapper">
         <h2>Tribes of Gymnocercus</h2>
-        <div class='headerButtonsWraper'>
-          <ul class='headerButtons'>
-            <li><button>Login</button></li>
-            <li><button>Register</button></li>
+        <div class="headerButtonsWraper">
+          <ul class="headerButtons">
+            <li>
+              <button>Login</button>
+            </li>
+            <li>
+              <button>Register</button>
+            </li>
           </ul>
         </div>
       </div>
-
     </div>
   );
 }
---
+++ frontend/src/Components/Login/resource.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+
+function Resources() {
+  const [food, setFood] = useState('');
+  const [gold, setGold] = useState('');
+  const [error, setError] = useState('');
+}
