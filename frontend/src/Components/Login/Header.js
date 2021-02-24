4634a86fbba1b737740c89a48a22d5cea4c5e268
Vona Csaba <vcsneu@gmail.com>
2021-02-24 10:07:57 +0100

    Revert "css egybeírása styles.css-be"
    This reverts commit 0e4d8a30133cfc17889a8d451cb914b9a0e06423.

--- frontend/src/Components/Login/Header.js
+++ frontend/src/Components/Login/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Header.css';
 function Header() {
   function handleSubmit(event) {
     event.preventDefault();
--- frontend/src/Components/Resource/Resource.js
+++ frontend/src/Components/Resource/Resource.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import '../Login/LoginForm.css';
 
 const port = process.env.PORT;
 console.log(process.env);
