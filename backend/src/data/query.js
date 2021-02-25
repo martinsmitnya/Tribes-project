458bec38dc17061db11b31a59572db63e95fa983
Vona Csaba <vcsneu@gmail.com>
2021-02-25 15:27:39 +0100

    Query végére ;

--- backend/src/data/query.js
+++ backend/src/data/query.js
@@ -1,3 +1,3 @@
-export const getResource = 'SELECT * FROM resources WHERE kingdomId=0';
+export const getResource = 'SELECT * FROM resources WHERE kingdomId=0;';
 export const postResource =
-  'UPDATE resources SET amount=?, updated_at=? WHERE type=? AND kingdomId=0';
+  'UPDATE resources SET amount=?, updated_at=? WHERE type=? AND kingdomId=0;';
