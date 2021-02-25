e886a6cb59f1dea139809f60569d308c684797d0
Vona Csaba <vcsneu@gmail.com>
2021-02-25 11:17:01 +0100

    kingdom -> kingdomId

--- backend/src/data/query.js
+++ backend/src/data/query.js
@@ -1,3 +1,3 @@
 export const getResource = 'SELECT * FROM resources';
 export const postResource =
-  'UPDATE resources SET amount=?, updated_at=? WHERE type=? kingdom=1';
+  'UPDATE resources SET amount=?, updated_at=? WHERE type=? AND kingdomId=1';
