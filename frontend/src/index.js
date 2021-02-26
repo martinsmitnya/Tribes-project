7ac24b32a76e90e684823466e64d65c3c6957017
Vona Csaba <vcsneu@gmail.com>
2021-02-26 09:22:03 +0100

    mappa átnevezés

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Resources from './Components/Resource/Resource';
+import Resources from './components/resource/Resource';
 
 ReactDOM.render(
   <React.StrictMode>
