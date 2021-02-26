7f065dce4d8aaac657f108310fbbf7ee6824e899
Martin Smitnya <smitnyamartin@gmail.com>
2021-02-26 09:40:52 +0100

    backend forloop, decapitalize folder letters

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import LoginPage from './Components/Login/LoginPage';
+import LoginPage from './components/login/LoginPage';
 
 ReactDOM.render(
   <React.StrictMode>
