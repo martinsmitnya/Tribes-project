88515608bc37dd017b18276277766e989c1b1878
jokutianna <jokutianna@gmail.com>
2021-03-03 17:26:26 +0100

    backend finished

--- backend/src/models/register.js
+++ backend/src/models/register.js
@@ -9,25 +9,15 @@ export async function registerNewUser(username, password, kingdom_name) {
     } else if (result.results.length === 0) {
       try {
         const insert = await db.query(`INSERT INTO users (username, passwordhash) VALUES (?,?);`, [username, password]);
-        //console.log(insert);
       }
       catch (err) {
         return { status: 401, error : 'Username and passwordhash not succesfully inserted'};
       }
       try {
-        //let mostRecentUserId = await getUserId;
         const selectUserId = await db.query(getUserId);
-        //console.log(selectUserId.results);
-        const add = await db.query(addKingdom, [kingdom_name, selectUserId.results[0].user_id]);
-        //console.log(selectUserId.results[0])
-        //console.log(add.results.insertId);
-        export const inesertedId = (add.results.insertId);
-        //console.log(inesertedId);
-
+        const add = await db.query(addKingdom, [kingdom_name, selectUserId.results[0].user_id])
       }
       catch (err) {
-        //console.log(err);
-        //console.log(db.query(getUserId).results);
         return { status: 401, error : 'Kingdom not succesfully added'};
       }
       return { status: 200, data: 'Success!' };
