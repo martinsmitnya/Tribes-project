854971062948a8e46a7abc9122985373cfefc15e
Vona Csaba <vcsneu@gmail.com>
2021-03-03 09:21:02 +0100

    fura conflictok

--- backend/src/routes/kingdom.routes.js
+++ backend/src/routes/kingdom.routes.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const cors = require('cors');
+<<<<<<< Updated upstream
 import { resourceController, buildingController } from '../controllers';
+=======
+import { resourceController, mapController } from '../controllers';
+>>>>>>> Stashed changes
 
 const router = express.Router();
 
@@ -9,5 +13,9 @@ router.use(express.json());
 
 router.get('/buildings', buildingController.get);
 router.get('/resource', resourceController.get);
+<<<<<<< Updated upstream
+=======
+router.get('/map', mapController.get);
+>>>>>>> Stashed changes
 
 export default router;
