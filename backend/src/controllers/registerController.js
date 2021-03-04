2bf14c6d68928ffddd6f48f0e8516b76eab143b3
Martin Smitnya <smitnyamartin@gmail.com>
2021-03-04 14:45:25 +0100

    sync function implemented

--- backend/src/controllers/registerController.js
+++ backend/src/controllers/registerController.js
@@ -4,7 +4,6 @@ import { passwordEncrypter } from '../middlewares/passwordEncrypter';
 export const registerController = {
   async post(req, res) {
     let encryptedPassword = await passwordEncrypter.encrypt(req.body.passwordhash);
-
     let data = await registerService.postRegister(
       req.body.username,
       encryptedPassword
--- backend/src/middlewares/passwordEncrypter.js
+++ backend/src/middlewares/passwordEncrypter.js
@@ -1,5 +1,10 @@
+const bcrypt = require('bcrypt');
+
 export const passwordEncrypter = {
-  async encrypt(password) {
-    return passwordHash
+  encrypt(password) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    return hash
   }
+
 }
