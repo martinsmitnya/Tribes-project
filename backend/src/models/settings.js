f3a97610d5b873fed81d5f7c20fc22831285a361
Lhaktoz <andi.horvath2@gmail.com>
2021-03-05 13:52:31 +0100

    working backend, added .env to gitignore

--- backend/src/models/settings.js
+++ backend/src/models/settings.js
@@ -1,13 +1,9 @@
 import { db } from '../data/connection';
 
 export async function updateKingdomName(kingdomName) {
-  try {
     const settingsquery = `UPDATE kingdoms SET kingdom_name = ? WHERE  id = 1;`;    
     const result = await db.query(settingsquery,[kingdomName]);
-    console.log(data.results)
-    return data.results;
-  } catch (error) {
-    console.log(error);
-    throw { message: 'database error', statusCode: 500}
-  }
+    console.log(result.results)
+    return result.results;
+ 
 }
--- backend/src/services/settingsService.js
+++ backend/src/services/settingsService.js
@@ -2,6 +2,10 @@ import { updateKingdomName } from '../models/settings';
 
 export const settingsService = {
   async putSettings(kingdomName) {
-    return await updateKingdomName(kingdomName);
+    if(!kingdomName) {
+      return {status: 400, error: "Name is required"}
+    } else {
+      return await updateKingdomName(kingdomName);
+    }
   },
 };
