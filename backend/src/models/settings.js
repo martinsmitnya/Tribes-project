48ad5f1cd25562a2619f2c6167ee863488dfd2ee
Lhaktoz <andi.horvath2@gmail.com>
2021-03-07 18:33:20 +0100

    kingdom id changed in query

--- backend/src/models/settings.js
+++ backend/src/models/settings.js
@@ -1,7 +1,7 @@
 import { db } from '../data/connection';
 
 export async function updateKingdomName(kingdomName) {
-    const settingsquery = `UPDATE kingdoms SET kingdom_name = ? WHERE  id = 1;`;    
+    const settingsquery = `UPDATE kingdoms SET kingdom_name = ? WHERE  id =33;`;    
     const result = await db.query(settingsquery,[kingdomName]);
     console.log(result.results)
     return result.results;
