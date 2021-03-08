88054b4e7e67e85e3c05a47abbd894f98fd7e0cd
Martin Smitnya <smitnyamartin@gmail.com>
2021-03-08 16:07:59 +0100

    password isFilled checcker added

--- backend/src/controllers/registerController.js
+++ backend/src/controllers/registerController.js
@@ -3,11 +3,16 @@ import { passwordEncrypter } from '../middlewares/passwordEncrypter';
 
 export const registerController = {
   async post(req, res) {
-    let encryptedPassword = await passwordEncrypter.encrypt(req.body.passwordhash);
-    let data = await registerService.postRegister(
-      req.body.username,
-      encryptedPassword
+    let data;
+    if (req.body.passwordhash) {
+      let encryptedPassword = await passwordEncrypter.encrypt(req.body.passwordhash);
+      data = await registerService.postRegister(
+        req.body.username,
+        encryptedPassword
       );
+    } else {
+      data = { status: 401, error: 'Error!' };
+    }
     res.status(data.status).json(data);
   },
 };
