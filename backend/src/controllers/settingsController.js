d21e495cdb68418134791967996be47343390b8d
Lhaktoz <andi.horvath2@gmail.com>
2021-03-08 15:43:23 +0100

    added querys in settings for detailed response

--- backend/src/controllers/settingsController.js
+++ backend/src/controllers/settingsController.js
@@ -3,8 +3,8 @@ import { settingsService } from '../services';
 export const settingsController = {
   async put(req, res) {
     try {
-      let data = await settingsService.putSettings(req.body.kingdomName);
-      res.status(200).json(data);
+      let data = await settingsService.putSettings(req.body.kingdom_name);
+      res.status(200).json(data.data);
     } catch(error) {
       console.log(error);
       res.status(500).json({ error: 'Database error' });
--- backend/src/models/settings.js
+++ backend/src/models/settings.js
@@ -1,9 +1,31 @@
 import { db } from '../data/connection';
 
+
 export async function updateKingdomName(kingdomName) {
-    const settingsquery = `UPDATE kingdoms SET kingdom_name = ? WHERE  id =33;`;    
+  try{
+    const settingsquery = `UPDATE kingdoms SET kingdom_name = ? WHERE  id = 39;`;    
     const result = await db.query(settingsquery,[kingdomName]);
-    console.log(result.results)
-    return result.results;
- 
+    const kingdomInfoQuery = `SELECT * FROM kingdoms WHERE id = 39`
+    const kingdomInfo = await db.query(kingdomInfoQuery);
+    const buildingInfoQuery = `SELECT building_id,type,level,hp,started_at,finished_at FROM buildings WHERE kingdomId=1;`
+    const buildingInfo = await db.query(buildingInfoQuery);
+    const resourceInfoQuery = `SELECT type, amount, generation FROM resources WHERE kingdomID = 1;;`
+    const resourceInfo = await db.query(resourceInfoQuery);
+    const troopsInfoQuery = `SELECT id, level, hp, attack, defence, started_at, finished_at FROM troops Where kingdom_id = 1;`
+    const troopsInfo = await db.query(troopsInfoQuery);
+    console.log(troopsInfo.results)
+
+    return {status: 200, data: {
+        "kingdom_id" : kingdomInfo.results[0].id,
+        "kingdom_name": kingdomInfo.results[0].kingdom_name,
+        "userId": kingdomInfo.results[0].user_id,
+        "buildings": buildingInfo.results,
+        "resources": resourceInfo.results,
+        "troops": troopsInfo.results,
+        "location": kingdomInfo.results[0].location,
+    }
+    };
+  } catch (err) {
+    return {status: 400, error:'Kingdomname not succesfully updated'}
+  }
 }
