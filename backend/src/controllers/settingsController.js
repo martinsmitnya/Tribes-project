898bd6170729e2a598ba57c3b47444f3638ae153
Lhaktoz <andi.horvath2@gmail.com>
2021-03-08 18:19:20 +0100

    throw error added in service and try-catch in controller

--- backend/src/controllers/settingsController.js
+++ backend/src/controllers/settingsController.js
@@ -3,11 +3,11 @@ import { settingsService } from '../services';
 export const settingsController = {
   async put(req, res) {
     try {
-      let data = await settingsService.putSettings(req.body.kingdom_name);
-      res.status(200).json(data.data);
-    } catch(error) {
-      console.log(error);
-      res.status(500).json({ error: 'Database error' });
+      let response = await settingsService.putSettings(req.body.kingdomName);
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      console.log(error.status)
+      res.status(error.status).json(error.message);
     }
   } 
 };
--- backend/src/services/settingsService.js
+++ backend/src/services/settingsService.js
@@ -3,7 +3,7 @@ import { updateKingdomName } from '../models/settings';
 export const settingsService = {
   async putSettings(kingdomName) {
     if(!kingdomName) {
-      return {status: 400, error: "Name is required"}
+      throw {status: 400, message: "Name is required"}
     } else {
       return await updateKingdomName(kingdomName);
     }
