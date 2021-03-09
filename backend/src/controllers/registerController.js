39fd2e50d240d53ce5540d62b2ef893b294e77b8
Martin Smitnya <smitnyamartin@gmail.com>
2021-03-09 10:43:16 +0100

    pw encryption In SERVICE layer

--- backend/src/controllers/registerController.js
+++ backend/src/controllers/registerController.js
@@ -1,20 +1,11 @@
 import { registerService } from '../services';
-import { passwordEncrypter } from '../middlewares/passwordEncrypter';
 
-//Legyen PW lekezelés?
-//Egyből titkosítsuk a PW-t vagy inkább a service layerben titkosítsuk?
 export const registerController = {
   async post(req, res) {
-    let data;
-    if (req.body.passwordhash) {
-      let encryptedPassword = await passwordEncrypter.encrypt(req.body.passwordhash);
-      data = await registerService.postRegister(
+      let data = await registerService.postRegister(
         req.body.username,
-        encryptedPassword
+        req.body.passwordhash
       );
-    } else {
-      data = { status: 401, error: 'Error!' };
-    }
     res.status(data.status).json(data);
   },
 };
--- backend/src/services/registerService.js
+++ backend/src/services/registerService.js
@@ -1,7 +1,15 @@
 import { registerNewUser } from '../models/register';
+import { passwordEncrypter } from '../middlewares/passwordEncrypter';
 
 export const registerService = {
-    async postRegister(username, passwordhash) {
-      return await registerNewUser(username, passwordhash);
-    },
+  async postRegister(username, password) {
+    let data;
+    if (password) {
+      let encryptedPassword = await passwordEncrypter.encrypt(password);
+      data = await registerNewUser(username, encryptedPassword)
+    } else {
+      data = { status: 401, error: 'Error!' };
+    }
+    return data
+  },
 };
