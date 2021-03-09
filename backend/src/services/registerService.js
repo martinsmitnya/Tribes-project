c6db6f173e1e3685a4f44b791eba5dac55b7cf57
Martin Smitnya <smitnyamartin@gmail.com>
2021-03-09 11:05:29 +0100

    pw encryption In SERVICE layer

--- backend/src/services/registerService.js
+++ backend/src/services/registerService.js
@@ -5,7 +5,9 @@ export const registerService = {
   async postRegister(username, password) {
     let data;
     if (password) {
+      //Create encryption
       let encryptedPassword = await passwordEncrypter.encrypt(password);
+      //Create encryption
       data = await registerNewUser(username, encryptedPassword)
     } else {
       data = { status: 401, error: 'Error!' };
