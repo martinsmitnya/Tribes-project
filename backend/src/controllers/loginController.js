908a782ca23c52196ca19d23817142556e2b2be8
Martin Smitnya <smitnyamartin@gmail.com>
2021-03-10 10:48:40 +0100

    throw error implemented

--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -5,7 +5,6 @@ export const loginController = {
     try{
       let serviceResponse = await loginService.postLogin(req.body);
       res.status(200).json(serviceResponse.data);
-     
     }catch(err){
       res.status(err.status).json(err.message);
     }
