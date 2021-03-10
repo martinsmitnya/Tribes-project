6a741491676d92dbbb6e2be8a4844a9d9f5ec1d4
Martin Smitnya <smitnyamartin@gmail.com>
2021-03-10 10:49:17 +0100

    throw error implemented

--- backend/src/services/loginService.js
+++ backend/src/services/loginService.js
@@ -5,27 +5,17 @@ export const loginService = {
 
     if (requestBody.username && !requestBody.password) {
       throw {status: 400, message: 'Password is required.'}
-      // return {
-      //   status: 400,
-      //   data: { 'status': 400, 'error': 'Password is required.' }
-      // }
+    
     } else if (!requestBody.username && requestBody.password) {
       throw {status: 400, message: 'Username is required.'}
-      // return {
-      //   status: 400,
-      //   data: { 'status': 400, 'error': 'Username is required.' }
-      // }
+     
     } else if (!requestBody.username && !requestBody.password) {
       throw {status: 400, message: 'All fields are required.'}
-      // return {
-      //   status: 400,
-      //   data: { 'status': 400, 'error': 'All fields are required.' }
-      // }
+   
     } else if (requestBody.username && requestBody.password) { /*When username and pw is here*/
       //run the token creator middleware
       let tokenResponse = await loginTokenCreator.tokenCreator(requestBody);
       return tokenResponse
     }
-
   }
 }
