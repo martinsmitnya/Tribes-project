bb1971f2fe2fd0abbd7d9a6903f49e49b42a4777
Martin Smitnya <smitnyamartin@gmail.com>
2021-03-12 14:55:51 +0100

    login adatok működnek, de register nem

--- backend/test/registerController.test.js
+++ backend/test/registerController.test.js
@@ -1,13 +1,67 @@
-import { registerController } from '../src/controllers/registerController';
+import request from 'supertest';
+import app from '../src/app';
+import { userRepository } from '../src/repositories/user'
+import { loginTokenCreator } from '../src/middlewares/loginTokenCreator'
 
-test('Register controller returns registering response', async()=> {
-  let input = {
-    "username" : "Martinka",
-    "password" : "jelszo123",
-    "kingdom_name" : "jozsikakiralysaga"
+jest.mock('../src/repositories/user');
+jest.mock('../src/middlewares/loginTokenCreator');
+
+const userTableData = {
+  username: 'Mate',
+}
+userRepository.getUserByUsernameAndPassword.mockResolvedValue(userTableData.username);
+loginTokenCreator.tokenCreator.mockResolvedValue(userTableData.username);
+
+test('Username', done => {
+  const inputData = {
+    username: 'NOTMate12312321',
   }
-  let res = '';
-  
-  let response = await registerController.post(input, res);
-  expect(response).toBe("Username is already taken");
-});
+
+  request(app)
+    .post('/login/login')
+    .send(inputData)
+    .set('Accept', 'application/json')
+    .expect('Content-type', 'application/json; charset=utf-8')
+    .expect(400)
+    .end((err, data) => {
+      if (err) return done(err);
+      expect(data.body).toEqual('Password is required.');
+      return done();
+    })
+})
+
+test('Password', done => {
+  const inputData = {
+    password: 'NOTMate12312321',
+  }
+
+  request(app)
+    .post('/login/login')
+    .send(inputData)
+    .set('Accept', 'application/json')
+    .expect('Content-type', 'application/json; charset=utf-8')
+    .expect(400)
+    .end((err, data) => {
+      if (err) return done(err);
+      expect(data.body).toEqual('Username is required.');
+      return done();
+    })
+})
+
+test('NO username OR password', done => {
+  const inputData = {
+    
+  }
+
+  request(app)
+    .post('/login/login')
+    .send(inputData)
+    .set('Accept', 'application/json')
+    .expect('Content-type', 'application/json; charset=utf-8')
+    .expect(400)
+    .end((err, data) => {
+      if (err) return done(err);
+      expect(data.body).toEqual('All fields are required.');
+      return done();
+    })
+})
