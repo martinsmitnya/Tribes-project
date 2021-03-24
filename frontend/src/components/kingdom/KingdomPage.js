3060b976f590c14dea7d54dc47d60457f0f2650a
daanworks <daniel.babinszky@gmail.com>
2021-03-24 14:10:54 +0100

    beautiful css fixes

--- frontend/src/components/kingdom/KingdomPage.js
+++ frontend/src/components/kingdom/KingdomPage.js
@@ -5,20 +5,27 @@ import Menu from '../menu/Menu';
 import Resources from '../resource/Resource';
 import Troops from '../troops/Troops';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './KingdomPage.css'
 
 const KingdomPage = () => {
 
   return (
-    <div>
+    <div className='kingdom-page-container'>
       <Header />
-      <Menu />
-      <Resources />
-      <Route path="/kingdom/buildings">
-        <Buildings />
-      </Route>
-      <Route path="/kingdom/troops">
-        <Troops />
-      </Route>
+      <div className='kingdom-page-main-container'>
+        <div className='menu-resources-container'>
+          <Menu />
+          <Resources />
+        </div>
+        <div className='main-content-container'>
+          <Route path="/kingdom/buildings">
+            <Buildings />
+          </Route>
+          <Route path="/kingdom/troops">
+            <Troops />
+          </Route>
+        </div>
+      </div>
     </div>
   );
 }
