93f810e900e9a81654f9d73e227b55bf969a5f5f
Vona Csaba <vcsneu@gmail.com>
2021-03-25 09:34:10 +0100

    Revert "login error javítva"
    This reverts commit e2ade51751d97e778fb60c1af765091cec5886a4.

--- backend/src/services/loginService.js
+++ backend/src/services/loginService.js
@@ -1,44 +1,41 @@
 import { loginTokenCreator } from '../middlewares/loginTokenCreator';
-import { userRepository } from '../repositories/user';
+import { userRepository } from '../repositories/user'
 const bcrypt = require('bcrypt');
 
 export const loginService = {
   async postLogin(requestBody) {
     if (requestBody.username && !requestBody.password) {
-      throw { status: 400, message: 'Password is required.' };
+      throw {status: 400, message: 'Password is required.'};
     } else if (!requestBody.username && requestBody.password) {
-      throw { status: 400, message: 'Username is required.' };
+      throw {status: 400, message: 'Username is required.'};
     } else if (!requestBody.username && !requestBody.password) {
-      throw { status: 400, message: 'All fields are required.' };
+      throw {status: 400, message: 'All fields are required.'};
     } else if (requestBody.username && requestBody.password) {
-      let userData = await userRepository.getUserByUsername(
-        requestBody.username
-      );
+      
+    let userData = await userRepository.getUserByUsername(
+      requestBody.username,
+    );
 
-      if (!userData) {
-        throw { status: 400, message: 'Wrong username or password' };
+    if (!userData) {
+      throw {status: 400, message: 'Wrong username'}
+    }
+
+    let validLogin = bcrypt.compareSync(requestBody.password,userData.passwordhash, (err,bool) => {
+      if (err) {
+        throw {status:500, message: 'Bcrypt is not working'}
       }
+    })
 
-      let validLogin = bcrypt.compareSync(
-        requestBody.password,
-        userData.passwordhash,
-        (err, bool) => {
-          if (err) {
-            throw { status: 500, message: 'Bcrypt is not working' };
-          }
-        }
-      );
+    if (!validLogin){
+      throw {status:400, message: 'Wrong username or password'};
+    }
 
-      if (!validLogin) {
-        throw { status: 400, message: 'Wrong username or password' };
-      }
-
-      try {
-        let tokenResponse = await loginTokenCreator.tokenCreator(requestBody);
-        return tokenResponse;
-      } catch (error) {
-        throw { status: 400, message: 'Failed to create Token' };
-      }
+    try {
+      let tokenResponse = await loginTokenCreator.tokenCreator(requestBody);
+      return tokenResponse;
+    } catch (error) {
+      throw { status: 400, message: 'Failed to create Token' };
+    }
     }
   },
 };
